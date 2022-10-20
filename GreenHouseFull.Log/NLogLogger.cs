05cc1d949ccce4515ff1eff2a1db50ebd414f876
Endalth <alican__karamaras@hotmail.com>
2022-10-20 12:01:40 +0300

    log

---
+++ GreenHouseFull.Log/NLogLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenHouseFull.Log
+{
+    public class NLogLogger<T> : ILogger<T> where T : class
+    {
+        private static readonly NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+        private string _logFileName;
+
+        public NLogLogger(string logFileName)
+        {
+            _logFileName = logFileName;
+        }
+
+        public void DoLog(T data)
+        {
+            _logger.Info("LogInfo: {logInfo}, LogFileName = {LogFileName}", data, _logFileName);
+        }
+    }
+}
